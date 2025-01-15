017e6bc51424d27388cec41730a863f5d7038f1f
Christopher Buckley <topherbuckley@gmail.com>
2025-01-15 15:03:37 +0900

    Updating example_record to use feedback rather than fixed time increments

--- arm/example_record_sim.py
+++ arm/example_record_sim.py
@@ -4,8 +4,6 @@ from beachbot.robot.vreprobotsimv1 import VrepRobotSimV1
 
 robot = VrepRobotSimV1(scene="roarm_m1_recorder_3finger.ttt")
 simarm = robot.arm
-timestep = 0.2
-increment = 0.025
 
 
 def record_to_npz(filename, timestamps, qs, taus):
@@ -17,8 +15,7 @@ def record_to_npz(filename, timestamps, qs, taus):
     )
 
 
-def pickup(log_file):
-    pathpospercent = 0.25
+def pickup(log_file, pathpospercent):
     i = -increment
     start_time = time.time()
     timestamps = []
@@ -31,14 +28,13 @@ def pickup(log_file):
         timestamps.append(current_time - start_time)  # Relative timestamp
         qs.append(q)
         taus.append(tau)
-        time.sleep(timestep)
         pathpospercent += i
-        simarm.set_target_path_pos(percent=pathpospercent, offset=[0, 0, 0])
+        simarm.set_target_path_pos(percent=pathpospercent)
+        wait_target_arrival()
 
     # Ensure the gripper matches the target state
     q[4] = 1
     simarm.set_joint_targets(q)
-    time.sleep(2)
 
     # Record final state
     current_time = time.time()
@@ -50,7 +46,7 @@ def pickup(log_file):
     record_to_npz(log_file, timestamps, qs, taus)
 
 
-def toss(log_file):
+def toss(log_file, pathpospercent):
     pathpospercent = 0.0
     i = increment
     start_time = time.time()
@@ -64,14 +60,13 @@ def toss(log_file):
         timestamps.append(current_time - start_time)  # Relative timestamp
         qs.append(q)
         taus.append(tau)
-        time.sleep(timestep)
         pathpospercent += i
         simarm.set_target_path_pos(percent=pathpospercent, offset=[0, 0, 0])
+        wait_target_arrival()
 
     # Ensure the gripper matches the target state
     q[4] = 0
     simarm.set_joint_targets(q)
-    time.sleep(3)
 
     # Record final state
     current_time = time.time()
@@ -83,6 +78,18 @@ def toss(log_file):
     record_to_npz(log_file, timestamps, qs, taus)
 
 
-simarm.go_home()
-pickup("pickup_log.npz")
-toss("toss_log.npz")
+def wait_target_arrival():
+    pos = simarm.get_gripper_pos()
+    target = simarm.get_gripper_target()
+    while np.linalg.norm(pos - target) > 0.02:
+        time.sleep(0.1)
+        pos = simarm.get_gripper_pos()
+        target = simarm.get_gripper_target()
+
+
+increment = 0.01
+pathpospercent = 0.25
+simarm.set_target_path_pos(pathpospercent)
+wait_target_arrival()
+pickup("pickup_log.npz", pathpospercent)
+toss("toss_log.npz", pathpospercent)
