b00a8ac735a561e3a95a3bb54e5f417640f5ca53
Christopher Buckley <topherbuckley@gmail.com>
2025-01-30 20:24:14 +0900

    Removing all the unncessary bulk from src/beachbot/control/pickupcontroller.py

--- src/beachbot/control/pickupcontroller.py
+++ src/beachbot/control/pickupcontroller.py
@@ -1,28 +1,10 @@
 from typing import List
-
 from ..robot.robotinterface import RobotInterface
 from .robotcontroller import RobotController, BoxDef
-from ..utils.controllercollection import PIDController
 
 
 class PickupController(RobotController):
     def __init__(self):
         super().__init__()
-        
 
-        example_default_float_var = 0.0
-
-
-        self._register_property("varnameExample", example_default_float_var)
-
-
-    def _property_changed_callback(self, name):
-        if name=="varnameExample":
-            print("var changed!!", self.get_property(name))
-        else:
-            super()._property_changed_callback(name)
-
-    def update(self, robot: RobotInterface, detections: List[BoxDef]=None, debug=False):
-          print("TODO pick up movement")
-        
-        
+    def update(self, robot: RobotInterface, detections: List[BoxDef] = None, debug=False):
