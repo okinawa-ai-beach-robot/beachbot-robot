c552ba512766303278bb4124aa3a7141943fc75f
Christopher Buckley <topherbuckley@gmail.com>
2025-01-31 20:05:58 +0900

    Revert "Adding threadpoolexecutor to pickupcontroller"
    This reverts commit 1b032cc2f3440744f65a667714c7429584015f20.

--- src/beachbot/control/pickupcontroller.py
+++ src/beachbot/control/pickupcontroller.py
@@ -2,18 +2,15 @@ from typing import List
 from beachbot.robot.robotinterface import RobotInterface
 from beachbot.control.robotcontroller import RobotController, BoxDef
 from beachbot.config import logger
-from concurrent.futures import ThreadPoolExecutor
 
 
 class PickupController(RobotController):
     def __init__(self):
         super().__init__()
-        self.executor = ThreadPoolExecutor(max_workers=1)
 
     def update(self, robot: RobotInterface, detections: List[BoxDef] = None):
         logger.info("pickup")
-        self.executor.submit(robot.arm.pickup)
+        robot.arm.pickup()
         logger.info("tossing")
-        self.executor.submit(robot.arm.toss)
-        self.executor.submit(robot.arm.go_home)
+        robot.arm.toss()
         return True
