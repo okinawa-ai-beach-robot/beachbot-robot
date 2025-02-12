fe36d89a5d0a79d2230c92906f9adedfc74de02e
Jeffrey <jeffrey-qeisser@oist.jp>
2025-02-12 15:59:18 +0900

    extended time history for controller plot to 30sec

--- app/beachbot_controller.py
+++ app/beachbot_controller.py
@@ -487,7 +487,7 @@ with tab_panel:
                     with ui.tab_panel(one_view):
                         video_image = ui.interactive_image().classes("w-full h-full")
                     with ui.tab_panel(two_view):       
-                        controller_plot = ui.line_plot(n=6, limit=60, figsize=(9, 6), update_every=5).with_legend(['Motor1', 'Motor2', 'vel_target', "vel_current", "rot_target", "rot_current"], loc='upper center', ncol=3).classes('w-full h-full border')
+                        controller_plot = ui.line_plot(n=6, limit=300, figsize=(9, 6), update_every=5).with_legend(['Motor1', 'Motor2', 'vel_target', "vel_current", "rot_target", "rot_current"], loc='upper center', ncol=3).classes('w-full h-full border')
     with ui.tab_panel(two):
         with ui.dropdown_button("Select File...", auto_close=True) as selector:
             pass
