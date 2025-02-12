1034250fcc84ef29191a99dc7f7e663cadd63e63
Jeffrey <jeffrey-qeisser@oist.jp>
2025-02-12 15:50:51 +0900

    threaded pickup

--- src/beachbot/control/pickupcontroller.py
+++ src/beachbot/control/pickupcontroller.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from typing import List
 from beachbot.robot.robotinterface import RobotInterface
 from beachbot.control.robotcontroller import RobotController, BoxDef 
@@ -8,13 +9,28 @@ from beachbot.config import logger
 class PickupController(RobotController):
     def __init__(self):
         super().__init__()
+        self.arm_thread=None
 
-    def update(self, robot: RobotInterface, detections: List[BoxDef] = None):
+    def operate_arm(self, robot: RobotInterface):
         logger.info("pickup")
         robot.arm.pickup()
         logger.info("tossing")
         robot.arm.toss()
         robot.arm.go_home()
 
+
+    def update(self, robot: RobotInterface, detections: List[BoxDef] = None):
+        if self.arm_thread is None:
+            # Create thread for amr movement, return, to not block controller loop
+            self.arm_thread = Thread(target=lambda r=robot:self.operate_arm(robot=r))
+            self.arm_thread.start()
+            return RESULT.BUSY
+        elif self.arm_thread.is_alive():
+            # Thread for arm movement is still running, so we are still busy
+            return RESULT.BUSY
+
+
+        # Done, we are not busy anymore with pickup, delete thread for arm movement
+        self.arm_thread = None
         #TODO assess situation and adjust return value appropriately, was pickup successful?
         return RESULT.SUCCESS
