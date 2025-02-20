b0bce0790f1c4716fb2877c6e0fe5e5467013012
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2025-02-20 23:40:45 +0900

    fancied-up recording script

--- utilities/record_sim_paths.py
+++ utilities/record_sim_paths.py
@@ -1,11 +1,10 @@
 import time
 import numpy as np
 from beachbot.robot.vreprobotsimv1 import VrepRobotSimV1
-from beachbot.manipulators.arm import normalize_gripper_angle
 from pathlib import Path
 
-robot = VrepRobotSimV1(scene="roarm_m1_recorder_3finger.ttt")
-simarm = robot.arm
+timeout_sec=2
+assets_path = "../src/beachbot/assets/"
 
 
 def record_to_npz(filename, timestamps, qs, taus):
@@ -19,7 +18,7 @@ def record_to_npz(filename, timestamps, qs, taus):
     )
 
 
-def pickup(trajectory_file, pathpospercent):
+def pickup(trajectory_file, pathpospercent, offset=[0,0,0], increment=0.01):
     i = -increment
     start_time = time.time()
     timestamps = []
@@ -33,8 +32,11 @@ def pickup(trajectory_file, pathpospercent):
         qs.append(q)
         taus.append(tau)
         pathpospercent += i
-        simarm.set_target_path_pos(percent=pathpospercent)
-        simarm.wait_target_pos_arrival()
+        pathpospercent = min(max(0,pathpospercent),1)
+        #pathpospercent, zero means pickup position, 1 means toss poisition
+        interpol_offset = [val*(1-pathpospercent) for val in offset]
+        simarm.set_target_path_pos(percent=pathpospercent, offset=interpol_offset)
+        simarm.wait_target_pos_arrival(timeout=timeout_sec)
 
     # Ensure the gripper matches the target state
     simarm.close_gripper()
@@ -49,7 +51,7 @@ def pickup(trajectory_file, pathpospercent):
     record_to_npz(trajectory_file, timestamps, qs, taus)
 
 
-def toss(trajectory_file, pathpospercent):
+def toss(trajectory_file, pathpospercent, offset=[0,0,0], increment=0.01):
     pathpospercent = 0.0
     i = increment
     start_time = time.time()
@@ -64,8 +66,11 @@ def toss(trajectory_file, pathpospercent):
         qs.append(q)
         taus.append(tau)
         pathpospercent += i
-        simarm.set_target_path_pos(percent=pathpospercent, offset=[0, 0, 0])
-        simarm.wait_target_pos_arrival()
+        pathpospercent = min(max(0,pathpospercent),1)
+        #pathpospercent, zero means pickup position, 1 means toss poisition
+        interpol_offset = [val*(1-pathpospercent) for val in offset]
+        simarm.set_target_path_pos(percent=pathpospercent, offset=interpol_offset)
+        simarm.wait_target_pos_arrival(timeout=timeout_sec)
 
     # Ensure the gripper matches the target state
     simarm.open_gripper()
@@ -79,13 +84,52 @@ def toss(trajectory_file, pathpospercent):
     record_to_npz(trajectory_file, timestamps, qs, taus)
 
 
+
+
+
+
+robot = VrepRobotSimV1(scene="roarm_m1_recorder_3finger.ttt")
+simarm = robot.arm
+
+print("start")
 increment = 0.01
 pathpospercent = 0.25
 simarm.open_gripper()
 simarm.set_target_path_pos(pathpospercent)
 simarm.wait_target_pos_arrival()
-assets_path = "src/beachbot/assets/"
 pickup_path = assets_path + "pickup.npz"
 toss_path = assets_path + "toss.npz"
-pickup(pickup_path, pathpospercent)
-toss(toss_path, pathpospercent)
+
+print("pickup")
+pickup(pickup_path, pathpospercent, increment=increment)
+print("toss")
+toss(toss_path, pathpospercent, increment=increment)
+
+print("Done recording default path")
+robot.cleanup()
+
+
+# record offset conditions for interpolation:
+print("Start recording offset paths")
+condition_dist = 0.10 # +/- 5cm
+
+for cond in [(-condition_dist,-condition_dist),(-condition_dist,condition_dist),(condition_dist,-condition_dist),(condition_dist,condition_dist)]:
+    x_off = cond[0]
+    y_off = cond[1]
+    offset_pickup = [x_off, y_off, 0]
+    print(f"Record for condition [{x_off},{y_off}] offset of target....")
+    #
+    robot = VrepRobotSimV1(scene="roarm_m1_recorder_3finger.ttt")
+    simarm = robot.arm
+    simarm.open_gripper()
+    simarm.set_target_path_pos(pathpospercent)
+    simarm.wait_target_pos_arrival()
+    
+    pickup_path = assets_path + f"pickup_{x_off*100:.0f}_{y_off*100:.0f}cm.npz"
+    toss_path = assets_path + f"toss_{x_off*100:.0f}_{y_off*100:.0f}cm.npz"
+    pickup(pickup_path, pathpospercent, increment=increment, offset=offset_pickup)
+    toss(toss_path, pathpospercent, increment=increment, offset=offset_pickup)
+    robot.cleanup()
+
+print(f"Done wit hall the fancy recrdings, stores in folder {assets_path}... see ya!")
+
