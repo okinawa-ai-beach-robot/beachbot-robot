39c758f48190ecbfbd30191481fc3705c371ac8e
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2025-03-09 23:57:51 +0900

    downscale variable added, 4x downscale AI -> 10Hz

--- src/beachbot/ai/yolov5_torch_hub.py
+++ src/beachbot/ai/yolov5_torch_hub.py
@@ -93,8 +93,9 @@ try:
         def apply_model(self, inputs, units_percent=True):
             self.net.conf = self.conf_threshold  # NMS confidence threshold
             row, col, _ = inputs.shape
+            downscaler=4
             with torch.no_grad():
-                results = self.net([inputs[..., ::-1]], size=row) # detect on BGR->RGB pixel format
+                results = self.net([inputs[..., ::-1]], size=round(row/downscaler)) # detect on BGR->RGB pixel format
 
             res = results.xyxy[0].numpy(force=True)
             result_class_ids = []
