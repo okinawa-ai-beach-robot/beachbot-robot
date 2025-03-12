c90313b581fffe87010de94b0a6492ccc1e26899
Jeffrey <jeffrey-qeisser@oist.jp>
2025-03-12 23:38:06 +0900

    Improved Impleentation of differnetial DriveSystem

--- src/beachbot/manipulators/drive.py
+++ src/beachbot/manipulators/drive.py
@@ -1,3 +1,4 @@
+from math import sqrt
 import time
 from beachbot.config import logger
 from beachbot.manipulators.motor import Motor
@@ -5,7 +6,21 @@ from beachbot.utils.properties import HasProperties
 import threading
 
 
+def safe_division(numerator, denominator):
+    """Return 0 if denominator is 0."""
+    return denominator and numerator / denominator
+
 def sign(x):
+    """Calculate sign of number. returns 0 if input is zero.
+
+    Args:
+        x (number): input value
+
+    Returns:
+        number: -1  if x<0
+                 1  if x>0
+                 0  else
+    """
     return (x > 0) - (x < 0)
 
 def bounded(val, mi=0, ma=1):
@@ -31,8 +46,7 @@ class DifferentialDrive(DriveSystem, threading.Thread):
 
         self._target_angular_vel = 0
         self._target_velocity = 0
-        self._current_angular_vel = 0
-        self._current_velocity = 0
+
 
 
         # The maximum value change per second of the contorl values (set_target)..
@@ -40,8 +54,16 @@ class DifferentialDrive(DriveSystem, threading.Thread):
         # Meant to avoid quick back-forth movements or other unrealistic accelerations by the robot
         self.max_rate_of_change = 400
 
+        # compensation of motor friction:
+        # scale 0-100% requested motor speed to pwm duty cycle self.friction_compensation-100 percent, motor speed of 0 results in 0 percent pwm duty cycle
+        # must be between 0 and <100
+        self.friction_compensation=15
+
         self._motor_left_speed = 0
         self._motor_right_speed = 0
+        self._target_motor_left_speed = 0
+        self._target_motor_right_speed = 0
+
 
 
         self.motor_left.change_speed(self._motor_left_speed)
@@ -53,7 +75,7 @@ class DifferentialDrive(DriveSystem, threading.Thread):
 
 
         self.register_property("max_rate_of_change", min_value=10, max_value=1000, descr="DifferentialDrive: The maximum rate of change of the control variables (rotation and velocity) per second. Acts as low pass filter for set_target(rot,vel) and avoids motor burn-out.")
-
+        self.register_property("friction_compensation", min_value=0, max_value=50, descr="compensation of motor friction, scale 0-100% requested motor speed to pwm duty cycle self.friction_compensation-100 percent, motor speed of 0 results in 0 percent pwm duty cycle")
 
         super().start()
 
@@ -81,51 +103,71 @@ class DifferentialDrive(DriveSystem, threading.Thread):
                     self.set_target(0, 0)
 
 
-            # estimate difference between current (last) motor command and targets (requested via set_target)
-            # dir menas how fast robot should rotate around own axis
-            # vel means how fast the robot should move forward
-            # See "set_targets" for more info
-            dir_delta = self._target_angular_vel - self._current_angular_vel
-            vel_delta = self._target_velocity - self._current_velocity
+
+            # Estimate motor velocity based on angluar and linear velocity (unbounded):
+            _target_motor_left_speed = self._target_velocity + 0.5 * self._target_angular_vel
+            _target_motor_right_speed = self._target_velocity - 0.5 * self._target_angular_vel
             
-            # Estimate the sign, i.e. is the new targets are smaller or lager as the current ones (-1 or 1)
-            dir_dir = sign(dir_delta)
-            vel_dir = sign(vel_delta)
 
-            # Move current motor signal toward the target, but limit the amount of value change to "self.max_rate_of_change" 
-            dir_dot = dir_dir * min(
-                last_dimediff*self.max_rate_of_change, abs(dir_delta)
-            )
+            # def f1(l,a):
+            #     m1 = l + 0.5*a
+            #     m2 = l - 0.5*a
+            #     desired_power = min(sqrt(a**2 + l**2),100)
+            #     unscaled_max_power = max(abs(m1), abs(m2))
+            #     scale_fac = safe_division(desired_power, unscaled_max_power)
+            #     m1 *= scale_fac
+            #     m2 *= scale_fac
 
-            vel_dot = vel_dir * min(
-                last_dimediff*self.max_rate_of_change, abs(vel_delta)
-            )
 
-            # Limit calue rage to -100 to 100 percent
-            self._current_angular_vel = bounded(
-                self._current_angular_vel + dir_dot, -100, 100
-            )
 
-            self._current_velocity = bounded(
-                self._current_velocity + vel_dot, -100, 100
-            )
 
-            # Estimate motor velocity based on angluar and linear velocity and update motors if changed:
-            __motor_left_speed = bounded(
-                self._current_velocity + 0.5 * self._current_angular_vel, -100, 100
-            )
 
-            __motor_right_speed = bounded(
-                self._current_velocity - 0.5 * self._current_angular_vel, -100, 100
-            )
+            # As we do not have velocity control (we control power, pwm duty cycle for now)
+            # we scale desired output such that total motor power is scaled in relation
+            # to euclidean distance of 2d control input (velocity and angular velocity) to its neutral point (0,0) 
+            # So distance of the joystick form center controls max motor power
+            # Range can be 0-100 percent max motor power:
+            #
+            # 1. desired_power based on, e.g. distance of joystick form center:
+            desired_power = min(sqrt(self._target_angular_vel**2 + self._target_velocity**2),100)
+            # 2. scaling to bound motor power to 100, but also to keep relative mottor values through scaling:
+            unscaled_max_power = max(abs(_target_motor_right_speed), abs(_target_motor_left_speed))
+            # 3. apply both scaling factors, also consider case unscaled_max_power=0 (velocity=0 and anglular_velocity=0) by 'save_division'
+            scale_fac = safe_division(desired_power, unscaled_max_power)
+            _target_motor_left_speed *= scale_fac
+            _target_motor_right_speed *= scale_fac
 
-            # +/-15 percent no motion ...
-            __motor_left_speed = bounded(
-                sign(__motor_left_speed) * 15 + __motor_left_speed, -100, 100
-            )
-            __motor_right_speed = bounded(
-                sign(__motor_right_speed) * 15 + __motor_right_speed, -100, 100
-            )
+
+            
+
+
+            # Rescale requested _target_motor_right_speed and _target_motor_left_speed to be in range self.friction_compensation..100 (positive or negative)
+            # if speed if zero, no change occurs (due to sign function)
+            _target_motor_left_speed = sign(_target_motor_left_speed) * self.friction_compensation + _target_motor_left_speed * (100-self.friction_compensation)/100
+            _target_motor_right_speed = sign(_target_motor_right_speed) * self.friction_compensation + _target_motor_right_speed * (100-self.friction_compensation)/100
+
+
+            # store in class, e.g. for plotting:
+            self._target_motor_left_speed = _target_motor_left_speed
+            self._target_motor_right_speed = _target_motor_right_speed
+
+
+
+            # change requested motor speed, limited through max rate of change:
+            # 1. estimate difference between current (last) motor command and new targets
+            motor_left_delta = _target_motor_left_speed - self._motor_left_speed
+            motor_right_delta = _target_motor_right_speed - self._motor_right_speed
+            # 2. estimate sign/direction of requested change in motor speed
+            motor_left_dir = sign(motor_left_delta)
+            motor_right_dir = sign(motor_right_delta)
+            # 3. estimate necessary change of motor values
+            motor_left_dot = motor_left_dir * min(last_dimediff*self.max_rate_of_change, abs(motor_left_delta))
+            motor_right_dot = motor_right_dir * min(last_dimediff*self.max_rate_of_change, abs(motor_right_delta))
+            # 4. update motor speed and bound to valid value ranges
+            __motor_left_speed = bounded(self._motor_left_speed + motor_left_dot, -100, 100)
+            __motor_right_speed = bounded(self._motor_right_speed + motor_right_dot, -100, 100)
+
+
 
             # Update motors if target values changed
             if self._motor_left_speed != int(__motor_left_speed):
