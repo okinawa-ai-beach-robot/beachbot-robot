049e54dc6845998177eae3b5d8aad7a790744577
Jeffrey <jeffrey-qeisser@oist.jp>
2025-03-26 14:38:36 +0900

    updated sim file branch name

--- src/beachbot/robot/vreprobotsimv1.py
+++ src/beachbot/robot/vreprobotsimv1.py
@@ -23,7 +23,7 @@ class VrepRobotSimV1(RobotInterface):
         github_download(self.scene_path,
                         config.BEACHBOT_HARDWARE_REPO,
                         "models/coppeliasim/" + scene,
-                        "sim_improvement",
+                        "hackathon",
                         overwrite=True
                         )
 
