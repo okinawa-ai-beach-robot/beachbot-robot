57af09c06330f05840db2a91ff38fb228688435f
Christopher Buckley <topherbuckley@gmail.com>
2025-03-27 18:15:53 +0900

    fixing formatting and removing duplicate info

--- src/beachbot/control/hackathon.py
+++ src/beachbot/control/hackathon.py
@@ -1,14 +1,15 @@
-from beachbot.control.controllerselector import ControllerSelector
-from beachbot.config import logger
-from beachbot.robot.robotinterface import RobotInterface
 from typing import List
-from beachbot.control.robotcontroller import BoxDef
+
+from beachbot.config import logger
+from beachbot.control.controllerselector import ControllerSelector
 from beachbot.control.robotcontroller import CONTROLLERRESULT as RESULT
+from beachbot.control.robotcontroller import BoxDef
+from beachbot.robot.robotinterface import RobotInterface
+
 
 class Hackathon(ControllerSelector):
     def __init__(self):
         super().__init__()
-
 
         # list of objects to be picked up:
         self.target_order = ["bottle", "cup", "sports ball"]
@@ -21,8 +22,8 @@ class Hackathon(ControllerSelector):
         self.approachDebris.targetfilter = self.current_target
 
     def update(self, robot: RobotInterface, detections: List[BoxDef] = None):
+        # 1.) If current control mode if approaching
         if self.controller is self.approachDebris:
-            # 1.) If current control mode if approaching
 
             # execute controller and test if it completed it's task:
             if self.controller.update(robot, detections) == RESULT.SUCCESS:
@@ -30,21 +31,18 @@ class Hackathon(ControllerSelector):
                 # select pickup controller as next controller:
                 self.controller = self.pickup
 
+        # 2.) If the current control mode if pick-up
         elif self.controller is self.pickup:
-            # 2.) If the current control mode if pick-up
 
             # execute controller pick-up and test if it is still busy with picking the object
             if self.controller.update(robot, detections) != RESULT.BUSY:
-                # if picking up is completed or failed
 
-                # Check if current_target no longer visible (not visible=picked up and placed in basked, if still visible it may dropped!)
+                # Check if current_target no longer visible (not visible=picked up and placed in basked, if still visible it was dropped!)
                 if not self.target_visible(detections):
 
-                    # Bottle disappeared! Consider this target successfully picked up
                     self.next_target()
-                
+
                 # if picking up failed or succeeded, continue with approaching objects again next!
                 self.controller = self.approachDebris
 
         return RESULT.BUSY
-
